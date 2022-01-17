3263355b9f4ccfd56d08857c1feab09a945c9e35
JustineEvansUM <justine.evans@mso.umt.edu>
2022-01-17 15:27:19 -0700

    move comptracking icons to new home

--- courses/art-sci-games/scripts/modules.js
+++ courses/art-sci-games/scripts/modules.js
@@ -10,31 +10,51 @@ var QUIZ = 'li.nodtype_quiz'
 var ASSN = 'li.modtype_assignment'
 var CHAT = 'li.modtype_hsuforum'
 
-/* Create URL+BOOK+H5P Module Group: */
-// Move URL title:
+/* Modify URL+BOOK+H5P Module Group: */
 $(this).ready(function(index) {
+  // Move URL title:
   $(this).find(URL).each(function(index) {
-    if ($(this).find('h3.snap-asset-link:contains("Information"),h3.snap-asset-link:contains("Walkthrough"),h3.snap-asset-link:contains("Tutorial")').length > 0) {
+      if ($(this).find('h3.snap-asset-link:contains("Information"),h3.snap-asset-link:contains("Walkthrough"),h3.snap-asset-link:contains("Tutorial")').length > 0) {
 
-      //-- 1. remove url overflow mask + other covers
-      $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
-      $(this).find(".snap-assettype").css("height", "0px");
-      $(this).find(".snap-header-card").css("display", "none");
-      $(this).find("p.snap-resource-card-fadeout").remove();
-      $(this).find("body.snap-resource-card li.snap-resource.modtype_url, body.snap-resource-card li.snap-resource").css("background-image","none");
+        //-- 1. remove url overflow mask + other covers
+        $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
+        $(this).find(".snap-assettype").css("height", "0px");
+        $(this).find(".snap-header-card").css("display", "none");
+        $(this).find("p.snap-resource-card-fadeout").remove();
+        $(this).find("body.snap-resource-card li.snap-resource.modtype_url").css("background-image", "none");
 
-      //-- 2. move module title to new home
-      let oldTitle = $(this).find(".snap-asset-link")
-      let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-      // save the title, remove it, and add to new location
-      oldTitle.remove();
-      newTitle.append(oldTitle);
+        //-- 2. move module title to new home
+        let oldTitle = $(this).find(".snap-asset-link")
+        let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+        // save the title, remove it, and add to new location
+        oldTitle.remove();
+        newTitle.append(oldTitle);
 
-      // -- 3. styling
-      // see linked stylesheet
-    };
+        //-- 3. move first set of module icons to new home
+        var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+        var newIcon = ".snap-asset-content .contentafterlink div.tile-completion"
+        // find the first of each, add empty class to mark
+        let oldIcon_URL = $(this).first().find(oldIcon).addClass("moved")
+        let oldIcon_BOOK = $(this).parent().find(BOOK).first().addClass("moved").find(oldIcon)
+        let oldIcon_H5P = $(this).parent().find(H5P).first().addClass("moved").find(oldIcon)
+        let newIconLocation = $(this).first().addClass("moved").find(newIcon)
+        // save the icons, remove them, and add to new location
+        oldIcon_URL.add(oldIcon_BOOK).add(oldIcon_H5P).remove()
+        newIconLocation.append(oldIcon_URL).append(oldIcon_BOOK).append(oldIcon_H5P);
+
+        //-- 3a. move next set of module icons to new home, repeat until finished
+        $(this).each(function() {
+          let oldIcon_URL = $(this).parent().find("li.modtype_url:not(.moved)").first().find(oldIcon).addClass("moved")
+          let oldIcon_BOOK = $(this).parent().find("li.modtype_book:not(.moved)").first().addClass("moved").find(oldIcon)
+          let oldIcon_H5P = $(this).parent().find("li.modtype_h5pactivity:not(.moved)").first().addClass("moved").find(oldIcon)
+          let newIconLocation = $(this).parent().find("li.modtype_url:not(.moved)").first().addClass("moved").find(newIcon)
+          // relocate all moved-targeted items to new location
+          newIconLocation.append(oldIcon_URL).append(oldIcon_BOOK).append(oldIcon_H5P);
+        });
+      });
   });
 });
+
 
 
 
