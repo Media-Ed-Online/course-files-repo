eee65f1715b7f704d4e042958cc0994257d23ed8
JustineEvansUM <justine.evans@mso.umt.edu>
2022-01-17 14:15:33 -0700

    update fontloader

--- courses/art-sci-games/scripts/modules.js
+++ courses/art-sci-games/scripts/modules.js
@@ -42,8 +42,9 @@ $(document).ready(function() {
 /* -------------
 | LINKED FONTS |
 ------------- */
+/* Font API script is loaded elsewhere (https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js). */
 WebFont.load({
   google: {
-      families: ['Fredoka+One']
+      families: ['Fredoka One']
   }
 });
