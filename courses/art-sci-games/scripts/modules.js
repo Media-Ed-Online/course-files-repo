eee5c51252cd7cb2bd5f393572ca1214e3091312
JustineEvansUM <justine.evans@mso.umt.edu>
2022-01-17 14:52:47 -0700

    update js with URL+BOOK+H5P grouping

--- courses/art-sci-games/scripts/modules.js
+++ courses/art-sci-games/scripts/modules.js
@@ -10,6 +10,32 @@ var QUIZ = 'li.nodtype_quiz'
 var ASSN = 'li.modtype_assignment'
 var CHAT = 'li.modtype_hsuforum'
 
+/* Create URL+BOOK+H5P Module Group: */
+$('ul.topics li').each(function() {
+ 
+  // Move URL title:
+  $(this).find(URL).each(function(index) {
+    if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '"),p.instancename:contains("Walkthrough")').length > 0) {
+
+      //-- 1. remove url overflow mask + other covers
+      $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
+      $(this).find(".snap-assettype").css("height", "0px");
+      $(this).find(".snap-header-card").css("display", "none");
+      $(this).find("p.snap-resource-card-fadeout").remove();
+      $(this).find("body.snap-resource-card li.snap-resource.modtype_url").css("background-image: none;");
+
+      //-- 2. move module title to new home
+      let oldTitle = $(this).find(".snap-asset-link")
+      let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+      // save the title, remove it, and add to new location
+      oldTitle.remove();
+      newTitle.append(oldTitle);
+
+      // -- 3. styling
+      // see linked stylesheet
+    };
+  });
+
 
 
 
