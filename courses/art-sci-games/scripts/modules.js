0aadc4e2327a016e5176bc31220a23428a59408e
JustineEvansUM <justine.evans@mso.umt.edu>
2022-01-22 15:49:52 -0700

    ***

--- courses/art-sci-games/scripts/modules.js
+++ courses/art-sci-games/scripts/modules.js
@@ -62,7 +62,7 @@ $(this).ready(function(index) {
 $(this).ready(function(index) {
   $(this).find(URL).each(function(index) {
     if ($(this).find('.snap-asset-content .contentafterlink div.tile-title p:contains("locked")').length > 0) {
-      $(this).parent().css("background-color", "pink");
+      $(this).css("background-color", "pink");
     };
   });
 });
