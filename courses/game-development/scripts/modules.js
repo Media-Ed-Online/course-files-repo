7a5b8e7fe7f3c634dcbe4cb20a124da30a7e5bc9
JustineEvansUM <justine.evans@mso.umt.edu>
2022-02-24 08:57:50 -0700

    modules css, move to jquery for :contains filter

---
+++ courses/game-development/scripts/modules.js
@@ -0,0 +1,18 @@
+/* ----------------------
+| MODULES JS OVERWRITES |
+---------------------- */
+
+/* Variables: */
+var URL = 'li.modtype_url'
+var BOOK = 'li.modtype_book'
+var H5P = 'li.modtype_h5pactivity'
+var QUIZ = 'li.nodtype_quiz'
+var ASSN = 'li.modtype_assignment'
+var CHAT = 'li.modtype_hsuforum'
+
+/* Modify URL+BOOK+H5P Module Group: */
+$(this).ready(function(index) {
+  // Move URL title:
+  $("nav#course-toc ol#chapters li:not(:contains('Exercise'))").css("border-top","1px solid #dee2e6;")
+  }
+});
