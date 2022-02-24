6d97fbfb614b8988dc9bc9eab194463d5e736aeb
JustineEvansUM <justine.evans@mso.umt.edu>
2022-02-24 12:11:03 -0700

    revert

--- courses/game-development/scripts/modules.js
+++ courses/game-development/scripts/modules.js
@@ -13,8 +13,5 @@ var CHAT = 'li.modtype_hsuforum'
 /* Modify URL+BOOK+H5P Module Group: */
 $(this).ready(function(index) {
   // Move URL title:
-  $('nav#course-toc ol#chapters li').css("border-top", "1px solid #dee2e6");
-  $('nav#course-toc ol#chapters li:contains("Exercise")').css({
-    "border-top": "none",
-  });
+  $('nav#course-toc ol#chapters li:not(:contains("Exercise"))').css("border-top", "1px solid #dee2e6");
 });
