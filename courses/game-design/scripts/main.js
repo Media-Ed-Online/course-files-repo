ee1394ad7cd5d6d81b8f8d7d31cbd9fe3afbfaa8
JustineEvansUM <justine.evans@mso.umt.edu>
2022-02-28 14:41:30 -0700

    design comp track toc

--- courses/game-design/scripts/main.js
+++ courses/game-design/scripts/main.js
@@ -15,6 +15,32 @@ $(window).ready(function(index) {
   $('#region-main').css('background', 'green');
 });
 
-$(window).on('hashchange',function(){ 
+$(window).on('hashchange',function(){
     window.location.reload(true);
+});
+
+
+
+/* ---------------------
+| COMPLETION TRACKING  |
+--------------------- */
+
+/* Replace tracking icons with custom images:*/
+$(document).ready(function() {
+  // Progress Tracking: Admin Indication
+  $('img[src*="completion-auto-enabled"]').attr("src", "https://media-ed-online.github.io/course-files-repo/courses/art-sci-games/graphics/comptrack/comptrack-incomplete.png");
+  // Progress Tracking: Incomplete
+  $('img[src*="completion-auto-n"]').attr("src", "https://media-ed-online.github.io/course-files-repo/courses/art-sci-games/graphics/comptrack/comptrack-incomplete.png");
+  // Progress Tracking: Complete
+  $('img[src*="completion-auto-y"]').attr("src", "https://media-ed-online.github.io/course-files-repo/courses/art-sci-games/graphics/comptrack/comptrack-complete-pass.png");
+  // Progress Tracking: Complete Pass
+  $('img[src*="completion-auto-pass"]').attr("src", "https://media-ed-online.github.io/course-files-repo/courses/art-sci-games/graphics/comptrack/comptrack-complete-pass.png");
+  // Progress Tracking: Complete Fail
+  $('img[src*="completion-auto-fail"]').attr("src", "https://media-ed-online.github.io/course-files-repo/courses/art-sci-games/graphics/comptrack/comptrack-complete-fail.png");
+
+  // Progress Tracking: Restriction Set
+  $('img[src*="lock"]').attr("src", "https://media-ed-online.github.io/course-files-repo/courses/art-sci-games/graphics/comptrack/comptrack-locked.png");
+
+  // Hide in Table of Contents:
+  $("nav#course-toc a.chapter-title + span.completionstatus").hide();
 });
