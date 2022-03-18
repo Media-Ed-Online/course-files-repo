eb92caa54537a5cfc5a555c91ba0ab73c32e1d59
JustineEvansUM <justine.evans@mso.umt.edu>
2022-03-18 08:19:50 -0600

    test

--- courses/game-design/scripts/main.js
+++ courses/game-design/scripts/main.js
@@ -15,3 +15,19 @@ $(this).ready(function(index) {
   // table of contents:
   $('nav#course-toc ol#chapters li:not(:contains("Discussion"))').css("border-top", "1px solid #dee2e6");
 });
+
+
+
+
+// Using the location.hash property to change the anchor part
+function changePart() {
+  location.hash = "topic-1";
+  var x = location.hash;
+  document.getElementById("demo").innerHTML = "The anchor part is now: " + x;
+}
+
+window.addEventListener("hashchange", myFunction);
+
+function myFunction() {
+  alert("The anchor part has changed!");
+}
