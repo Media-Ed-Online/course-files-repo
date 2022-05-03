0dca6d03a0afed33bf11ea20fef94d4f91228ace
JustineEvansUM <justine.evans@mso.umt.edu>
2022-05-03 14:57:25 -0600

    revert

--- courses/art-sci-games-new/scripts/main.js
+++ courses/art-sci-games-new/scripts/main.js
@@ -15,11 +15,3 @@ $(this).ready(function(index) {
   // table of contents:
   $('nav#course-toc ol#chapters li:not(:contains("Exercise"))').css("border-top", "1px solid #dee2e6");
 });
-
-
-
-
-// Using the location.hash property to change the anchor part
-if (window.location.href.indexOf("#section-3") > -1) {
-  alert("The anchor part has changed!");
-}
