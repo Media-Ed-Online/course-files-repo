c2621538a16bb4455225e845d3da531027ee0a02
JustineEvansUM <justine.evans@mso.umt.edu>
2022-05-08 19:08:00 -0600

    section numbers

--- courses/art-sci-games-new/scripts/main.js
+++ courses/art-sci-games-new/scripts/main.js
@@ -15,5 +15,5 @@ $(document).ready(function(index) {
   // table of contents:
   $('nav#course-toc ol#chapters li:not(:contains("Exercise"))').css("border-top", "1px solid #dee2e6");
 
-  $('div').parentsUntil('section#region-main').find('div.course-content ul.topics li.section h2.sectionname span.sectionnumber').hide();
+  $('div#moodle-blocks').parentsUntil('section#region-main').find('div.course-content ul.topics li.section h2.sectionname span.sectionnumber').hide();
 });
