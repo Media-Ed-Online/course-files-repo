fb54ec51c0dfe9e79571fc26af952e92312a3b9c
JustineEvansUM <justine.evans@mso.umt.edu>
2022-05-16 15:50:57 -0600

    add prev()

--- courses/art-sci-games-new/scripts/main.js
+++ courses/art-sci-games-new/scripts/main.js
@@ -25,7 +25,7 @@ $('body').on('DOMNodeInserted', function(e) {
 
   /* Modify URL+BOOK+H5P Module Group: */
   // Group URL+BOOK+H5P together:
-  $(this).prev().nextUntil('li.section').ready(function() {
+  $(this).prevUntil('li.section').nextUntil('li.section').ready(function() {
     let a = $('li.modtype_url .snap-asset-link a').text();
     let b = $('li.modtype_book h3.snap-asset-link').text();
 
