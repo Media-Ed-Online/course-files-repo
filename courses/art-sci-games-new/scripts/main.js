73a61c608bfe3d1a634c24248b7f9bfebe5d203e
JustineEvansUM <justine.evans@mso.umt.edu>
2022-06-05 16:47:41 -0600

    try getbyclassname

--- courses/art-sci-games-new/scripts/main.js
+++ courses/art-sci-games-new/scripts/main.js
@@ -38,7 +38,7 @@ $(document).ready(function(index) {
 });*/
 
 // Select the node that will be observed for mutations
-const targetNode = document.getElementById('ul.topics');
+const targetNode = document.getElementsByClassName('ul.topics');
 
 // Options for the observer (which mutations to observe)
 const config = { attributes: true, childList: true, subtree: true };
@@ -48,7 +48,7 @@ const callback = function(mutationsList, observer) {
     // Use traditional 'for loops' for IE 11
     for(const mutation of mutationsList) {
         if (mutation.type === 'childList') {
-              $('li.section h2.sectionname span.sectionnumber').hide();
+            console.log('A child node has been added or removed.');
         }
         else if (mutation.type === 'attributes') {
             console.log('The ' + mutation.attributeName + ' attribute was modified.');
