d02fcbb6d15e34e36ef87e6aecc3f87fbb4763cc
JustineEvansUM <justine.evans@mso.umt.edu>
2022-06-05 16:34:51 -0600

    test mutation

--- courses/art-sci-games-new/scripts/main.js
+++ courses/art-sci-games-new/scripts/main.js
@@ -37,12 +37,30 @@ $(document).ready(function(index) {
   alert(filteredArray)
 });*/
 
-const observer = new MutationObserver(function(mutations_list) {
-	mutations_list.forEach(function(mutation) {
-		mutation.addedNodes.forEach(function(added_node) {
-			$('li.section h2.sectionname span.sectionnumber').hide();
-				observer.disconnect();
-		});
-	});
-});
-observer.observe(document.querySelector("li"), { subtree: false, childList: true });
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('li.section');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+              $('li.section h2.sectionname span.sectionnumber').hide();
+        }
+        else if (mutation.type === 'attributes') {
+            console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+observer.disconnect();
