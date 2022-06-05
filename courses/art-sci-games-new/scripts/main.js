a47d729c0683ac1d40fab9e9c12482236bb595e6
JustineEvansUM <justine.evans@mso.umt.edu>
2022-06-05 17:30:27 -0600

    var to let

--- courses/art-sci-games-new/scripts/main.js
+++ courses/art-sci-games-new/scripts/main.js
@@ -24,11 +24,11 @@ const observeTopicsList = document.querySelector("ul.topics li");
 const observer = new MutationObserver(function(e) {
   $('li.section h2.sectionname span.sectionnumber').hide();
 
-  var data1 = $.find(" > li.modtype_url p.instancename").map(function(index) {
+  let data1 = $.find(" > li.modtype_url p.instancename").map(function(index) {
     //return index + ": " + $(this).text();
     return $(this).text()
   }).get();
-  var data2 = $.find(" > li.modtype_book p.instancename").map(function(index) {
+  let data2 = $.find(" > li.modtype_book p.instancename").map(function(index) {
     return $(this).text();
   }).get();
 
