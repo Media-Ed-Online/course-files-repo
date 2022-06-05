41a0a1c5be7cec860fde99b592e7055c70ff16f6
JustineEvansUM <justine.evans@mso.umt.edu>
2022-06-05 17:00:12 -0600

    test event listener

--- courses/art-sci-games-new/scripts/main.js
+++ courses/art-sci-games-new/scripts/main.js
@@ -37,34 +37,8 @@ $(document).ready(function(index) {
   alert(filteredArray)
 });*/
 
-
-
-
-
-// Select the node that will be observed for mutations
-const targetNode = document.getElementsByClassName('topics');
-
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for(const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            console.log('A child node has been added or removed.');
-        }
-        else if (mutation.type === 'attributes') {
-            console.log('The ' + mutation.attributeName + ' attribute was modified.');
-        }
+document.addEventListener('animationstart', function(event){
+    if (event.animationName == 'nodeInserted'){
+        $('li.section h2.sectionname span.sectionnumber').hide();
     }
-};
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
-
-// Later, you can stop observing
-observer.disconnect();
+}, true);
