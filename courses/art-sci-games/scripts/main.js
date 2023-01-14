0c6a9f00df0ae0c400e764f81f48a23443d8180a
JustineEvansUM <justine.evans@mso.umt.edu>
2023-01-14 14:53:18 -0700

    added styling and script

---
+++ courses/art-sci-games/scripts/main.js
@@ -0,0 +1,31 @@
+/* -------------------
+| MAIN JS OVERWRITES |
+------------------- */
+
+/* Variables: */
+var URL = 'li.modtype_url'
+var BOOK = 'li.modtype_book'
+var H5P = 'li.modtype_h5pactivity'
+var QUIZ = 'li.nodtype_quiz'
+var ASSN = 'li.modtype_assignment'
+var CHAT = 'li.modtype_hsuforum'
+
+/* Main Components: */
+$(document).ready(function(index) {
+  // table of contents, separate sections:
+  $('nav#course-toc ol#chapters li:not(:contains("Exercise"))').css("border-top", "1px solid #dee2e6");
+});
+
+/* DOM Changes & Observations: */
+const observeTopicsList = document.querySelector("ul.topics li");
+
+const observer = new MutationObserver(function() {
+  $('li.section h2.sectionname span.sectionnumber').hide();
+});
+
+observer.observe(observeTopicsList, {
+  subtree: true,
+  childList: true
+});
+
+/* Stop Videos When Modal Closes: */
